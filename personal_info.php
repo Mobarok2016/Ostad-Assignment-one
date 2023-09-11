e72c46cd287f50f5d7653ed232105288be350174
unknown <mobarokhossain448@gmail.com>
2023-09-11 21:05:10 +0600

    first commit

---
+++ personal_info.php
@@ -0,0 +1,13 @@
+
+<?php
+
+$name = "Mobarok Hossan";
+$age = 25;
+$country = "Bangladesh";
+$introduction= "I am ${name}, a graduate from shahjalal university of science and technology.I am ${age} years old.<br> I reside in the district of Brahmanbaria in ${country}. Now I am learning PHP and Laravel to build my career in the IT field.";
+
+echo "<h2> Name : $name </h2>";
+echo "<h2>Age : $age</h2>";
+echo "<h2> Country : $country </h2>";
+echo "<p> Brief Description : ${introduction} </p>";
+
