5be7ca9bced80235adb2403f6aa60b4bda08a657
unknown <mobarokhossain448@gmail.com>
2023-09-12 08:47:34 +0600

    weather report added

---
+++ weather_report.php
@@ -0,0 +1,17 @@
+<?php 
+
+$temperature=12;
+
+if($temperature>=30){
+    echo "It's warm";
+}
+
+else if($temperature>=10){
+    echo "It's cool";
+}
+
+else if($temperature<10){
+    echo "It's freezing";
+}
+
+?>
